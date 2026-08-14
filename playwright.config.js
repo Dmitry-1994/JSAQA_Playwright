365b2c2f34f385ead29ebc0ed7d6946106430af4
Dmitry Tarasov <vizalyyy@gmail.com>
2026-08-15 00:46:23 +0500

    Работа над ошибками

--- playwright.config.js
+++ playwright.config.js
@@ -24,21 +24,67 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: "html",
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         launchOptions: {
             headless: false,
-            slowMo: 5000
+            slowMo: 1000
         },
         /* Base URL to use in actions like `await page.goto('/')`. */
         // baseURL: 'http://127.0.0.1:3000',
 
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry"
     },
-    timeout: 150000,
+    expect: {
+        timeout: 10000 // Время в миллисекундах (в данном примере — 10 секунд)
+    },
+
+    // Глобальный таймаут на один весь тест целиком (опционально)
+    timeout: 60000,
+
+    /* Configure projects for major browsers */
     projects: [
         {
             name: "chromium",
             use: { ...devices["Desktop Chrome"] }
         }
+
+        // {
+        //     name: "firefox",
+        //     use: { ...devices["Desktop Firefox"] }
+        // },
+
+        // {
+        //     name: "webkit",
+        //     use: { ...devices["Desktop Safari"] }
+        // }
+
+        /* Test against mobile viewports. */
+        // {
+        //   name: 'Mobile Chrome',
+        //   use: { ...devices['Pixel 5'] },
+        // },
+        // {
+        //   name: 'Mobile Safari',
+        //   use: { ...devices['iPhone 12'] },
+        // },
+
+        /* Test against branded browsers. */
+        // {
+        //   name: 'Microsoft Edge',
+        //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+        // },
+        // {
+        //   name: 'Google Chrome',
+        //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+        // },
     ]
+
+    /* Run your local dev server before starting the tests */
+    // webServer: {
+    //   command: 'npm run start',
+    //   url: 'http://127.0.0.1:3000',
+    //   reuseExistingServer: !process.env.CI,
+    // },
 });
